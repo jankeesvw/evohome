f57a605c0a1b37ddb4deeab2cfb773041baa9e6b
Jankees van Woezik <mail@jankeesvw.com>
2017-12-09 13:37:43 +0100

    Move fixtures folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
 
   VCR.configure do |config|
-    config.cassette_library_dir = "fixtures/vcr_cassettes"
+    config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
     config.hook_into :webmock
   end
 
