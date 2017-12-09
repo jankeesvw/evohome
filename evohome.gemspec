6828953e8038d10155a36ee200f153cfa48dae2d
Jankees van Woezik <mail@jankeesvw.com>
2017-12-09 13:30:53 +0100

    Make a connection with the API

--- evohome.gemspec
+++ evohome.gemspec
@@ -32,4 +32,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "vcr", "~> 4.0"
+  spec.add_development_dependency "webmock", "~> 3.1"
+  spec.add_dependency "httparty", "~> 0.15.6"
 end
--- lib/evohome.rb
+++ lib/evohome.rb
@@ -1,7 +1,23 @@
 require "evohome/version"
+require "httparty"
 
 class Evohome
-  def initialize
-    puts "Hello Evohome."
+  attr_reader :sessionid
+
+  def initialize(username:, password:, application_id:)
+    @username = username
+    @password = password
+    @application_id = application_id
+  end
+
+  def connect!
+    response = HTTParty.post("https://tccna.honeywell.com/WebAPI/api/Session",
+                             body: { "username" => @username,
+                                     "password" => @password,
+                                     "ApplicationId" => @application_id }.to_json,
+                             headers: { "Content-Type" => "application/json" })
+
+    @sessionid = response["sessionId"]
+    self
   end
 end
